cc7e025859da24d1619c8cb5205cb145e7d4a7a8
Kellis <vikiclashonyt@gmail.com>
2020-01-30 10:21:44 +0000

    android actions

---
+++ Candidat 4/site/android/actions/add_vrc.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+$bdd = new PDO('mysql:host=localhost;dbname=vrc', 'root', '');
+
+if(isset($_GET['addvrc'])) {
+    if(isset($_SESSION['id'])) {
+        if(!empty($_GET['name']) AND !empty($_GET['digit'])) {
+            $name = htmlspecialchars($_GET['name']);
+            $digit = htmlspecialchars($_GET['digit']);
+
+            $reqdigit = $bdd->prepare("SELECT * FROM vrc WHERE id = ?");
+            $reqdigit->execute(array($digit));
+            $digitexist = $reqdigit->rowCount();
+
+            if ($digitexist == 0) {
+                $insertmbr = $bdd->prepare('INSERT INTO vrc(name, digit, user_id, date) VALUES(?, ?, ?, UNIX_TIMESTAMP())');
+                $insertmbr->execute(array($_GET['name'], $_GET['digit'], $_SESSION['id']));
+                echo "ok";
+            }
+            else {
+                echo "L'ID de cette VRC est déjà inscrite. Veuillez choisir un autre ID";
+            }
+        } 
+        else {
+            echo "Formulaire incomplet";
+        }
+    }
+    else {
+        echo "Vous n'êtes pas connecté";
+    }
+}
+else {
+    echo "Erreur";
+}
+?>
---
+++ Candidat 4/site/android/actions/login.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+$bdd = new PDO('mysql:host=localhost;dbname=vrc', 'root', '');
+if(isset($_GET['login'])){
+    if(!empty($_GET['username']) AND !empty($_GET['password'])) {
+        $username = htmlspecialchars($_GET['username']);
+        $password = md5($_GET['password']);
+
+        $requser = $bdd->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
+        $requser->execute(array($username, $password));
+        $userexist = $requser->rowCount();
+        $reqmail = $bdd->prepare("SELECT * FROM users WHERE mail = ? AND password = ?");
+        $reqmail->execute(array($username, $password));
+        $mailexist = $reqmail->rowCount();
+        if($userexist == 1 OR $mailexist == 1) {
+            $userinfo = $requser->fetch();
+            $_SESSION['id'] = $userinfo['id'];
+            $requser = $bdd->prepare("UPDATE users SET ip = ? WHERE id = ?");
+            $requser->execute(array($_SERVER['REMOTE_ADDR'], $userinfo['id']));
+
+            $requsernamefr = $bdd->prepare("SELECT * FROM users WHERE username = ?");
+            $requsernamefr->execute(array($username));
+            
+            $usernameexistfr = $requsernamefr->fetch();
+
+            $_SESSION['id'] = $usernameexistfr['id'];
+            if(isset($_SESSION['id'])) {
+                echo 'ok';
+            } else {
+                echo "Erreur de connexion";
+            }
+        } else {
+            echo "Vous n'êtes pas enregistré dans nos bases de données. Veuillez vous inscrire s'il vous plaît.";
+        }
+    } else {
+        echo "Tous les champs doivent être remplis.";
+    }
+} else { 
+    echo "Erreur";
+}
+?>
