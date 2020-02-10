143f7a548c3458fba4a507b8cd1058b2b577fdfe
Kellis <vikiclashonyt@gmail.com>
2020-02-10 12:22:26 +0100

    Added C++ Arduino chrono software

---
+++ Candidat 4/Arduino/chrono/chrono.ino
@@ -0,0 +1,45 @@
+int count = 0;
+unsigned long milli, milli2 = 0, liste[100] = {};
+
+void setup()
+{
+  Serial.begin(9600);
+}
+  
+enum EtatCourant {
+   Initialisation,
+   AttenteReponse,
+} etat = Initialisation;
+  
+void loop()
+{
+  switch ( etat ) {
+     case Initialisation:
+        etat = AttenteReponse;
+     break;
+  
+     case AttenteReponse:
+         switch ( Serial.read() ) {
+             case 'C':                
+                //millis() = Le nombre de millisecondes depuis que le programme est lancé
+                milli2 = milli2 + milli;
+                //milli2 = Le somme totale des millisecondes depuis que le programme est lancé
+                milli = millis() - milli2;
+                //milli = Le nombre de millisecondes entre chaque clicks
+                liste[count] = milli;
+                //liste[] = La liste des milliseconds
+                                
+                Serial.println(liste[count]);
+                count++;
+                etat = AttenteReponse;
+             break;
+             case -1:     // rien reçu il faut attendre
+             case '\n':   // ignorer terminateur
+              break;
+             default:
+                Serial.println("Erreur saisie");
+             break;
+         }
+     break;
+    }
+}
